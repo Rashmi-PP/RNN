55c066f0e31137067cbad91fbe6a0d0c6d2b0b6e
Rashmi PP <62315650+Rashmi-PP@users.noreply.github.com>
2022-07-07 22:41:04 -0400

    Update and rename code.py to code.ipynb

--- code.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.keras.preprocessing import sequence
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Embedding
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras.datasets import imdb
